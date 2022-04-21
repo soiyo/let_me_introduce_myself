41222ed09ef2247710baa90d559beb608ba9189e
soiyo <soiyosauce@gmail.com>
2022-04-21 18:03:22 +0900

    team-project-ver.4

--- team-project-1/app.py
+++ team-project-1/app.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 client = MongoClient('클러스터 URL')
 db = client.team
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,17 +13,41 @@ def home():
 def getpeople1():
     return render_template('personal_test_1.html')
 
+@app.route('/gittin') 
+def gittin(): return redirect("https://github.com/kti0940")
+
+@app.route('/blgtin')
+def blgtin(): return redirect("https://velog.io/@kti0940")
+
 @app.route('/getPerson2')
 def getpeople2():
     return render_template('personal_test_2.html')
+
+@app.route('/gitfr') 
+def gitfr(): return redirect("https://github.com/KimmyJay")
+
+@app.route('/blgfr') 
+def blgfr(): return redirect("https://frannyk.tistory.com/")
 
 @app.route('/getPerson3')
 def getpeople3():
     return render_template('personal_test_3.html')
 
+@app.route('/gitmun') 
+def gitmun(): return redirect("https://github.com/ansaudwn1234")
+
+@app.route('/blggmun') 
+def blgmun(): return redirect("https://velog.io/@ansaudwn123")
+
 @app.route('/getPerson4')
 def getpeople4():
     return render_template('personal_test_4.html')
+
+@app.route('/gitgh') 
+def gitgh(): return redirect("https://github.com/soiyo")
+
+@app.route('/blggh') 
+def blggh(): return redirect("https://velog.io/@soyoyun")
 
 
 @app.route("/profile", methods=["POST"])
